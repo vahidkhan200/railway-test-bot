8fff453c0a2bf14be71549d414c49c3de9f87d5e
vahidkhan200 <vahidelder200@gmail.com>
2025-04-14 11:08:47 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import requests
 
-url = "https://api.lbank.info/v2/ticker.do?symbol=btc_usdt"
-response = requests.get(url)
-data = response.json()
+symbols = [
+    'btc_usdt', 'eth_usdt', 'xrp_usdt', 'bnb_usdt',
+    'hbar_usdt', 'xlm_usdt', 'xdc_usdt', 'not_usdt', 'dydx_usdt'
+]
 
-price = data['data'][0]['ticker']['latest']
-print("BTC/USDT:", price)
+for symbol in symbols:
+    url = f"https://api.lbank.info/v2/ticker.do?symbol={symbol}"
+    response = requests.get(url)
+    data = response.json()
+
+    try:
+        price = data['data'][0]['ticker']['latest']
+        print(f"{symbol.upper()}: {price}")
+    except Exception as e:
+        print(f"Error fetching {symbol}: {e}")
