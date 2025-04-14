7bb8d641944aa10ad2d89a5290b492a9f999eb54
vahidkhan200 <vahidelder200@gmail.com>
2025-04-14 10:48:50 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello from railway!")
+import requests
+
+url = "https://api.lbank.info/v2/ticker.do?symbol=btc_usdt"
+res = requests.get(url)
+data = res.json()
+
+price = data['ticker']['latest']
+print("BTC/USDT:", price)
