2f529ffab30f597e781adf4389985c2e9aa14c20
vahidkhan200 <vahidelder200@gmail.com>
2025-04-14 11:04:49 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,7 @@ import requests
 
 url = "https://api.lbank.info/v2/ticker.do?symbol=btc_usdt"
 response = requests.get(url)
+data = response.json()
 
-print("Status Code:", response.status_code)
-
-try:
-    data = response.json()
-    print("Raw JSON:", data)
-except Exception as e:
-    print("JSON parsing error:", e)
-    print("Raw text:", response.text)
+price = data['data'][0]['ticker']['latest']
+print("BTC/USDT:", price)
