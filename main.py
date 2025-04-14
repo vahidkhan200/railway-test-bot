b0a5dc6ceb9298e5d133133f80787b3f0746304a
vahidkhan200 <vahidelder200@gmail.com>
2025-04-14 10:53:24 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,4 @@ url = "https://api.lbank.info/v2/ticker.do?symbol=btc_usdt"
 res = requests.get(url)
 data = res.json()
 
-price = data['ticker']['latest']
-print("BTC/USDT:", price)
+print("Raw response:", data)
