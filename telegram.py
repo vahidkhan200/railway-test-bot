19e4a1aa1ed7ff4ed0a4b405d381b5cf88697049
vahidkhan200 <vahidelder200@gmail.com>
2025-04-15 10:10:19 +0330

    Create telegram.py

---
+++ telegram.py
@@ -0,0 +1,13 @@
+import requests
+from config import TELEGRAM_TOKEN, CHAT_ID
+
+def send_message_to_telegram(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
+
+    response = requests.post(url, data=payload)
+    return response.json()
