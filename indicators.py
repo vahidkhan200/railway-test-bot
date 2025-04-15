803ccc91b29b4d3e5600816bf6506b3497145083
vahidkhan200 <vahidelder200@gmail.com>
2025-04-15 10:18:12 +0330

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,1 +1,28 @@
+import pandas as pd
+import ta
 
+def add_indicators(klines):
+    df = pd.DataFrame(klines)
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+    df.set_index('timestamp', inplace=True)
+
+    # EMA
+    df['EMA_20'] = ta.trend.ema_indicator(df['close'], window=20).ema_indicator()
+
+    # RSI
+    df['RSI'] = ta.momentum.RSIIndicator(df['close'], window=14).rsi()
+
+    # MACD
+    macd = ta.trend.MACD(df['close'])
+    df['MACD'] = macd.macd()
+    df['MACD_signal'] = macd.macd_signal()
+
+    # ATR
+    df['ATR'] = ta.volatility.AverageTrueRange(
+        high=df['high'],
+        low=df['low'],
+        close=df['close'],
+        window=14
+    ).average_true_range()
+
+    return df
