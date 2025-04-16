8d9130508d98b01edbc841e7c923516a83ee8520
vahidkhan200 <vahidelder200@gmail.com>
2025-04-16 13:03:04 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,2 @@
-import os
-from dotenv import load_dotenv
-
-# بارگذاری متغیرهای محیطی از فایل .env
-load_dotenv()
-
-# گرفتن توکن تلگرام و شناسه چت از متغیرهای محیطی
-TELEGRAM_TOKEN = os.getenv("7808088088:AAGu9D1Vr5Iq6lrrE7P2jbMr32_-K6Y8wF4")
-CHAT_ID = os.getenv("392018191")
+TELEGRAM_TOKEN = "7808088088:AAGu9D1Vr5Iq6lrrE7P2jbMr32_-K6Y8wF4"
+TELEGRAM_CHAT_ID = "392018191"
